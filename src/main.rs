afb5bec4347ffcda79556dd141a97335e9856413
robertwoodward <robertjwoodward@outlook.com>
2024-04-11 17:34:10 +1000

    Initial

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,9 @@ mod get_args;
 use crate::get_args::{get_paths};
 
 fn main() {
-    let args = get_paths();
-    let src_path = args.source_dir_path.as_str();
-    let tgt_path = args.target_dir_path.as_str();
+    let paths = get_paths();
+    let src_path = paths.source_dir_path.as_str();
+    let tgt_path = paths.target_dir_path.as_str();
 
     remove_extras::remove_extra_dirs_and_files(tgt_path, src_path);
     create_dirs::create_missing_dirs(src_path, tgt_path);
